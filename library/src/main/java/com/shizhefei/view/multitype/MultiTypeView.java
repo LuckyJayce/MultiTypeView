1b389b9413e00dcd0cf3760a5316d863fc92d5ad
LuckyJayce <794629068@qq.com>
2016-08-13 11:51:49 +0800

    MultiTypeView改成直接继承RecyclerView

--- library/src/main/java/com/shizhefei/view/multitype/MultiTypeView.java
+++ library/src/main/java/com/shizhefei/view/multitype/MultiTypeView.java
@@ -16,22 +16,17 @@ limitations under the License.
 package com.shizhefei.view.multitype;
 
 import android.content.Context;
-import android.graphics.PointF;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.LinearSmoothScroller;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
-import android.util.DisplayMetrics;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
 
 /**
  * Created by LuckyJayce on 2016/8/11.
  */
-public class MultiTypeView extends FrameLayout {
-    private RecyclerView recyclerView;
+public class MultiTypeView extends RecyclerView {
+
     private MultiTypeAdapter<?> typeAdapter;
 
     public MultiTypeView(Context context) {
@@ -49,75 +44,29 @@ public class MultiTypeView extends FrameLayout {
         init(context);
     }
 
-    public class MyCustomLayoutManager extends LinearLayoutManager {
-        private static final float MILLISECONDS_PER_INCH = 50f;
-        private Context mContext;
-
-        public MyCustomLayoutManager(Context context) {
-            super(context);
-            mContext = context;
-        }
-
-        @Override
-        public void smoothScrollToPosition(RecyclerView recyclerView,
-                                           RecyclerView.State state, final int position) {
-
-            LinearSmoothScroller smoothScroller =
-                    new LinearSmoothScroller(mContext) {
-
-                        //This controls the direction in which smoothScroll looks
-                        //for your view
-                        @Override
-                        public PointF computeScrollVectorForPosition
-                        (int targetPosition) {
-                            return MyCustomLayoutManager.this
-                                    .computeScrollVectorForPosition(targetPosition);
-                        }
-
-                        //This returns the milliseconds it takes to
-                        //scroll one pixel.
-                        @Override
-                        protected float calculateSpeedPerPixel
-                        (DisplayMetrics displayMetrics) {
-                            return MILLISECONDS_PER_INCH/displayMetrics.densityDpi;
-                        }
-                    };
-
-            smoothScroller.setTargetPosition(position);
-            startSmoothScroll(smoothScroller);
-        }
-    }
-
     private void init(Context context) {
-        recyclerView = new RecyclerView(context);
-        addView(recyclerView, new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-        recyclerView.setId(ViewUtils.madeId());
-        recyclerView.setLayoutManager(new MyCustomLayoutManager(context));
+        setLayoutManager(new LinearLayoutManager(context));
     }
 
-    public RecyclerView getRecyclerView() {
-        return recyclerView;
+    @Override
+    public void setAdapter(Adapter adapter) {
+        if (adapter instanceof MultiTypeAdapter) {
+            setAdapter((MultiTypeAdapter) adapter);
+        } else {
+            super.setAdapter(adapter);
+        }
     }
 
     public void setAdapter(MultiTypeAdapter<?> adapter) {
         this.typeAdapter = adapter;
-        this.recyclerView.setAdapter(typeAdapter);
-    }
-
-    public RecyclerView.LayoutManager getLayoutManager() {
-        return recyclerView.getLayoutManager();
-    }
-
-    public MultiTypeAdapter<?> getAdapter() {
-        return typeAdapter;
+        super.setAdapter(typeAdapter);
     }
 
     @Override
     protected Parcelable onSaveInstanceState() {
         Bundle stateBundle = new Bundle();
-        MultiTypeAdapter adapter = getAdapter();
-        if (adapter != null) {
-            adapter.onSaveInstanceState(this, stateBundle);
+        if (typeAdapter != null) {
+            typeAdapter.onSaveInstanceState(this, stateBundle);
         }
         stateBundle.putParcelable("superParcelable", super.onSaveInstanceState());
         return stateBundle;
@@ -127,14 +76,12 @@ public class MultiTypeView extends FrameLayout {
     protected void onRestoreInstanceState(Parcelable state) {
         if (state instanceof Bundle) {
             Bundle stateBundle = (Bundle) state;
-            MultiTypeAdapter adapter = getAdapter();
-            if (adapter != null) {
-                adapter.onRestoreInstanceState(this, stateBundle);
+            if (typeAdapter != null) {
+                typeAdapter.onRestoreInstanceState(this, stateBundle);
             }
             super.onRestoreInstanceState(stateBundle.getParcelable("superParcelable"));
         } else {
             super.onRestoreInstanceState(state);
         }
     }
-
 }
